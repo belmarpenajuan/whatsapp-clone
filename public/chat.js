fcbd924680734a474ef0b23638215290e0b8cf28
Juan Belmar Peña <juanbelmarpena@MacBook-Air-de-Juan.local>
2026-06-22 19:24:50 -0400

    fix: autenticacion de firebase y limpieza de scripts

--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,17 @@
+   const firebaseConfig = {
+    apiKey: "AIzaSyD6DpcPWdYbRWsgLHfcpy6kNuYXeL6SZUU",
+    authDomain: "whatsapp-2-53dcf.firebaseapp.com",
+    projectId: "whatsapp-2-53dcf",
+    storageBucket: "whatsapp-2-53dcf.firebasestorage.app",
+    messagingSenderId: "902728389072",
+    appId: "1:902728389072:web:75dff0fd2db5cc463de9d5",
+    measurementId: "G-N6GZFT0M6X"
+    };
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
 const socket = io(); 
 
 
